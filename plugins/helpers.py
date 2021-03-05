fc20eb56eda4944715affe600a5a52739bc7d4be
rajaganapathy2000 <79167443+rajaganapathy2000@users.noreply.github.com>
2021-03-06 01:32:22 +0530

    Update helpers.py

--- plugins/helpers.py
+++ plugins/helpers.py
@@ -25,7 +25,7 @@ async def progress_for_pyrogram(
 
         progress = "[{0}{1}] \n\n📊Progress: {2}%\n".format(
             ''.join(["█" for i in range(math.floor(percentage / 5))]),
-            ''.join([" " for i in range(20 - math.floor(percentage / 5))]),
+            ''.join(["░" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
         tmp = progress + "\n✅ Completed: {0}\n\n📁 Total Size: {1}\n️\n🚀Speed: {2}/s\n\n⌚️ETA: {3}\n".format(
